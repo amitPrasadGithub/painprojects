5d085a107d5cc4c77ed1b65d86469b9b8acce4e8
amitPrasadGithub <52345552+amitPrasadGithub@users.noreply.github.com>
2019-07-02 18:00:10 +0530

    Added ResultSetMetaData changes

--- DBConnection.java
+++ DBConnection.java
@@ -8,11 +8,16 @@ public class DBConnection {
 	String user = "root";
 	String pass = "proxy";
 	
+	Scanner sc = null;
+	
 	Connection con = null;
 	PreparedStatement pstmnt = null;
+	Statement stmnt  = null;
+	StringBuffer sb = null;
 	
 	public void registerDriver(String driver) throws Exception {
 		System.out.print("\n enter which driver to load 1 or 2 :: ");
+		System.out.print("\n 1 :: Class.forName \n 2 :: DriverManager.registerDriver ");
 		int choice = new Scanner(System.in).nextInt();
 		switch(choice) {
 			case 1:
@@ -31,10 +36,61 @@ public class DBConnection {
 		con = DriverManager.getConnection(url, user, pass);
 		System.out.print("\n ## connction done");
 	}
+	public void createTable() throws Exception {
+		sb = new StringBuffer("create table");
+		sc = new Scanner(System.in);
+		System.out.print("\n Table Name>");
+		String tableName = sc.nextLine();
+		sb.append(" "+tableName+" (");
+		
+		System.out.print("\n How many attributes>");
+		int size = sc.nextInt();
+		String[] attr = new String[size];
+		for(int i=0; i<size; i++) {
+			System.out.print("\n Attribute>");
+			attr[i] = sc.nextLine();
+			if(i==0)
+				sb.append(" "+attr[i]);
+			else
+				sb.append(", "+attr[i]);
+			
+		}
+		sb.append(");");
+	}
+	public void dropTable() throws Exception {
+		System.out.print("\n Table Name to drop>");
+		String tableName = new Scanner(System.in).nextLine();
+		String sql = "drop table "+tableName+";";
+		stmnt = con.createStatement();
+		boolean b = stmnt.execute(sb.toString());
+	}
+	public void select() {
+		String sql = "select * from contact";
+		
+		try {
+			stmnt = con.createStatement();
+			ResultSet rs = stmnt.executeQuery(sql);
+			ResultSetMetaData md = rs.getMetaData();
+			int cols = md.getColumnCount();
+			String[] str = new String[6];
+			for(int i=1; i<=cols; i++) {
+				str[i] = md.getColumnName(i);
+				System.out.print("\n  :: "+i+" = "+md.getColumnName(i));
+			}
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		System.out.print("\n Enter driver name to register :: ");
-		new DBConnection().registerDriver(new Scanner(System.in).nextLine());
-		new DBConnection().getConnection();
+		DBConnection d = new DBConnection();
+		d.registerDriver(new Scanner(System.in).nextLine());
+		d.getConnection();
+		//new DBConnection().dropTable();
+		d.select();
 		
 	}
 	
