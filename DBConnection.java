11a9cd75ffe0e5dad440559ccb49444c3cbb449d
amitPrasadGithub <52345552+amitPrasadGithub@users.noreply.github.com>
2019-07-03 14:08:10 +0530

    Added insert update delete and propertie file

--- DBConnection.java
+++ DBConnection.java
@@ -1,17 +1,23 @@
 import java.util.*;
 import java.sql.*;
-import sun.jdbc.odbc.*;
+import java.io.*;
 
 
 public class DBConnection {
+	
+	public static final String INSERT = "insert";
+	public static final String UPDATE = "update";
+	public static final String DELETE = "delete";
+	public static final String SELECT = "select";
+	
 	String url 	= "jdbc:mysql://localhost:3306/vbesfb";
 	String user = "root";
-	String pass = "proxy";
+	String pass = "CDSWEB";
 	
 	Scanner sc = null;
 	
 	Connection con = null;
-	PreparedStatement pstmnt = null;
+	PreparedStatement pstmnt;;
 	Statement stmnt  = null;
 	StringBuffer sb = null;
 	
@@ -33,8 +39,26 @@ public class DBConnection {
 		
 	}
 	public void getConnection() throws Exception {
-		con = DriverManager.getConnection(url, user, pass);
-		System.out.print("\n ## connction done");
+		System.out.print("\n 1> Instance variables  \n 2> Properties File ");
+		System.out.print("\n Choice  :: ");
+		int choice = new Scanner(System.in).nextInt();
+		switch(choice) {
+			case 1:
+				con = DriverManager.getConnection(url, user, pass);
+				System.out.print("\n ## Connction done");
+				break;
+			case 2: 
+				Properties p = new Properties();
+				p.load(new FileInputStream("DB.properties"));
+				con = DriverManager.getConnection(url, p);
+				System.out.print("\n ## Connection done");
+				System.out.print("\n key = "+p.getProperty("user")+"\n key = "+p.getProperty("password"));
+				
+				break;
+			default:
+				break;
+		}
+		
 	}
 	public void createTable() throws Exception {
 		sb = new StringBuffer("create table");
@@ -56,6 +80,52 @@ public class DBConnection {
 			
 		}
 		sb.append(");");
+	}
+	
+	public void delete() throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("\n ## Inside DELETE");
+		String sql = "delete from contact where contactId = ?";
+		pstmnt = con.prepareStatement(sql);
+		while (true) {
+			System.out.print("\n Enter contactId to delete => ");
+			pstmnt.setInt(1, new Scanner(System.in).nextInt());
+			
+			int i = pstmnt.executeUpdate();
+			
+			System.out.print("\n Enter 0 to exit.. OR any other key to continue del");
+			if(sc.nextInt() == 0) 
+				System.exit(0);
+		}
+	}
+	public void insert() throws Exception {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\n ## Inside INSERT");
+		String sql = "insert into contact (contactId, fname, lname, addressId, phoneId, webaddressId) values(?, ?, ?, ?, ?, ?)";
+		pstmnt = con.prepareStatement(sql);
+		while(true) {
+			System.out.print("\n Enter fname => ");
+			pstmnt.setString(2, sc.nextLine());
+			
+			System.out.print("\n Enter contactId => ");
+			pstmnt.setInt(1, sc.nextInt());
+			
+			
+			System.out.print("\n Enter lname => ");
+			pstmnt.setString(3, sc.nextLine());
+			System.out.print("\n Enter addressId => ");
+			pstmnt.setInt(4, sc.nextInt());
+			System.out.print("\n Enter phoneId => ");
+			pstmnt.setInt(5, sc.nextInt());
+			System.out.print("\n Enter webaddressId => ");
+			pstmnt.setInt(6, sc.nextInt());
+			
+			int i=pstmnt.executeUpdate();
+			System.out.print("\n Press 0 to exit...");
+			if(sc.nextInt() == 0) 
+				System.exit(0);
+		}	
 	}
 	public void dropTable() throws Exception {
 		System.out.print("\n Table Name to drop>");
@@ -84,13 +154,34 @@ public class DBConnection {
 			e.printStackTrace();
 		}
 	}
+	public String getTableName() throws Exception {
+		return "";
+	}
+
 	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
 		System.out.print("\n Enter driver name to register :: ");
 		DBConnection d = new DBConnection();
 		d.registerDriver(new Scanner(System.in).nextLine());
 		d.getConnection();
-		//new DBConnection().dropTable();
-		d.select();
+		
+		System.out.print("\n Operation => ");
+		String operation = sc.nextLine();
+		switch(operation) {
+			case DBConnection.INSERT:
+				d.insert();
+				break;
+			case DBConnection.UPDATE:
+				//new DBConnection().update();
+				break;
+			case DBConnection.DELETE:
+				d.delete();
+				break;
+			case DBConnection.SELECT:
+				d.select();
+			defalult:
+				break;
+		}
 		
 	}
 	
